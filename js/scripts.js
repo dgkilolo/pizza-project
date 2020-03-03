b1197a8df1673aeb95a6c75b7fffff32330f42b3
David <dgkilolo23@gmail.com>
2020-03-03 03:17:44 +0300

    adds the calculations feature allowing to user to see the total cost of the pizza and the crust.

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,27 @@ $(document).ready(function() {
     var yourCrust = $("#crust1").val();
     var yourToppings = $("#exampleCheck1").val();
     $("#yourOrder").text(yourOrder +", "+ yourCrust +", "+ yourToppings)
-    alert(yourOrder + yourCrust +yourToppings);  
+    //alert(yourOrder + yourCrust +yourToppings); 
+    
+    if (yourOrder == "Large 1200/=" ) {
+      var sizeCost = 1200;
+    } else if (yourOrder == "Medium 800/=") {
+      var sizeCost = 800;
+    }  else {
+      var sizeCost = 600;
+    }
+
+    if (yourCrust == "Thin crust 50/=") {
+      var crustCost = 50;
+    } else if (yourCrust == "Deep crust 150/=") {
+      var crustCost = 150;
+    } else {
+      var crustCost = 200;
+    }
+        
+    $("#yourTotal").text(sizeCost+crustCost+"ksh")
+    alert(sizeCost+crustCost)
+
   });
 
   $('#confirm').click(function() {    
