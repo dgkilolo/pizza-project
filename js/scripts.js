adcd43191759bf4e78c83f7d17304b4f2c54f9a0
David <dgkilolo23@gmail.com>
2020-03-03 04:22:21 +0300

    adds correct indentation and spacing to the js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
-$(document).ready(function() {
- 
+$(document).ready(function() { 
   $('#delivery').click(function(listen) {
     $('.delivery').toggle(); 
-    var deliveryCost = 100;
-
-    
+    var deliveryCost = 100;    
   });
-
-  $('#order').click(function() {
-    
+  $('#order').click(function() {    
     var yourOrder =$("#sizes1").val();
     var yourCrust = $("#crust1").val();
-    var yourToppings = $("#exampleCheck1").val();
-    $("#yourOrder").text(yourOrder +", "+ yourCrust +", "+ yourToppings)
-    //alert(yourOrder + yourCrust +yourToppings); 
-    
+    var yourToppings = $("#topping").val();
+    $("#yourOrder").text(yourOrder +", "+ yourCrust )    
+    if (yourToppings == "on") {
+      var toppingCost = 80;
+    }    
     if (yourOrder == "Large 1200/=" ) {
       var sizeCost = 1200;
     } else if (yourOrder == "Medium 800/=") {
@@ -22,7 +18,6 @@ $(document).ready(function() {
     }  else {
       var sizeCost = 600;
     }
-
     if (yourCrust == "Thin crust 50/=") {
       var crustCost = 50;
     } else if (yourCrust == "Deep crust 150/=") {
@@ -30,39 +25,15 @@ $(document).ready(function() {
     } else {
       var crustCost = 200;
     }
-
-    $("#yourTotal").text(sizeCost+crustCost+"ksh")
+    $("#yourTotal").text(sizeCost+crustCost+toppingCost+"ksh")
     alert(sizeCost+crustCost)
-
-  
-
-  $('#confirm').click(function() {  
-    
-    $('.finalTotal').show(); 
-    
+  $('#confirm').click(function() {     
+    $('.finalTotal').show();     
     var confirm =$("#inputAddress").val();   
     alert("Your pizza will be delivered to " + confirm);  
-
-    $("#finalTotal").text(sizeCost+crustCost+100+"ksh")
-
-    
+    $("#finalTotal").text(sizeCost+crustCost+toppingCost+100+"ksh")     
   });
-
-  });
-
-  
-
-  
-  
-
-  // function handle(event) {
-  //   //event.preventDefault();
-  //   var delivery = document.getElementById("exampleFormControlSelect1")
-  //   alert("Your pizza will be delivered to " + delivery )
-  // }
-  
-
-
+  });  
 });
 
 
